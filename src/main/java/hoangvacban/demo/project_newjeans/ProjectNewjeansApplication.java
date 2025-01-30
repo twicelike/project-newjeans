425f4928c8a5630159dd5f7a98ce2423ff48323e
just_flash_kit <mhoanga1@gmail.com>
2025-01-30 21:56:33 +0700

    first commit

---
+++ src/main/java/hoangvacban/demo/project_newjeans/ProjectNewjeansApplication.java
@@ -0,0 +1,13 @@
+package hoangvacban.demo.project_newjeans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectNewjeansApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectNewjeansApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hoangvacban/demo/project_newjeans/ProjectNewjeansApplicationTests.java
@@ -0,0 +1,13 @@
+package hoangvacban.demo.project_newjeans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectNewjeansApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
