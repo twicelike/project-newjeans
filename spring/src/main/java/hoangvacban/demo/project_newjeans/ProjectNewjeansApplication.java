3fbb8f9beb4eadbdd561d8b24a4bb3aef9517bf9
just_flash_kit <mhoanga1@gmail.com>
2025-01-31 19:40:06 +0700

    set up project

--- spring/src/main/java/hoangvacban/demo/project_newjeans/ProjectNewjeansApplication.java
+++ spring/src/main/java/hoangvacban/demo/project_newjeans/ProjectNewjeansApplication.java
@@ -2,7 +2,6 @@ package hoangvacban.demo.project_newjeans;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
 public class ProjectNewjeansApplication {
