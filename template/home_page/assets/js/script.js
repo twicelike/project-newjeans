156787201d19b2ed18e90b5f34969f2ca60cba5c
Thinh Nguyen <hongthinhnguyen789@gmail.com>
2025-03-31 19:39:38 +0700

    Update Home Page

--- template/home_page/assets/js/script.js
+++ template/home_page/assets/js/script.js
@@ -11,3 +11,72 @@ const interactIcon = document.getElementById('interactIcon');
 interactIcon.addEventListener('click', function(){
     interactIcon.classList.toggle('interact-icon');
 });
+
+//Up/Down Button
+document.addEventListener('DOMContentLoaded', function () {
+  const posts = document.querySelectorAll('.main-wrapper > div');
+  let currentIndex = 0;
+
+  // Hàm hiển thị bài viết theo index
+  function showPost(index) {
+    posts.forEach((post, i) => {
+      if (i === index) {
+        post.classList.remove('hidden');
+      } else {
+        post.classList.add('hidden');
+      }
+    });
+  }
+
+  const upButton = document.querySelector('.up-button');
+  const downButton = document.querySelector('.down-button');
+
+  //Up Button
+  if (upButton) {
+    upButton.addEventListener('click', () => {
+      if (currentIndex > 0) {
+        currentIndex--;
+        showPost(currentIndex);
+      }
+    });
+  }
+
+  //Down Button
+  if (downButton) {
+    downButton.addEventListener('click', () => {
+      if (currentIndex < posts.length - 1) {
+        currentIndex++;
+        showPost(currentIndex);
+      }
+    });
+  }
+
+  //Cuộn chuột
+  const mainWrapper = document.querySelector('.root');
+  let isScrolling = false;
+
+  mainWrapper.addEventListener('wheel', (e) => {
+    e.preventDefault();
+
+    if (isScrolling) return;
+    isScrolling = true;
+
+    if (e.deltaY > 0) {
+      // Cuộn xuống
+      if (currentIndex < posts.length - 1) {
+        currentIndex++;
+        showPost(currentIndex);
+      }
+    } else {
+      // Cuộn lên
+      if (currentIndex > 0) {
+        currentIndex--;
+        showPost(currentIndex);
+      }
+    }
+
+    setTimeout(() => {
+      isScrolling = false;
+    }, 100);
+  });
+});
