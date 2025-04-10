ddcdd55f3bdc6fac09327278f4974a2658840cff
Do Huy <doquochuy16905@gmail.com>
2025-04-10 15:42:24 +0700

    formalization

--- template/register/js/SwitchingWindow.js
+++ template/register/js/SwitchingWindow.js
@@ -1,12 +1,16 @@
-const nextSetup = document.getElementById("nextSetup");
+const nextImagesSetup = document.getElementById("nextImagesSetup");
+const returnImagesSetup =document.getElementById("backImagesSetup");
+const nextHobbiesSetup = document.getElementById("nextHobbiesSetup");
 const returnSetup = document.getElementById("backSetup");
 const addImages = document.getElementById("addImages");
+const addHobbies = document.getElementById("addHobbies");
 const setUpProfileForm = document.getElementById("setUpProfileForm");
 const setUpBasicInfo = document.getElementById("setUpBasicInfo");
 
-nextSetup.onclick = switchSetUp;
+nextImagesSetup.onclick = switchSetUp;
 function switchSetUp() {
   setUpProfileForm.classList.add("SlideAndFadeM2L");
+  // console.logerror("asd");
   setTimeout(() => {
     setUpBasicInfo.classList.add("hidden");
     setUpProfileForm.classList.remove("SlideAndFadeM2L");
@@ -15,6 +19,20 @@ function switchSetUp() {
   setTimeout(() => {
     setUpProfileForm.classList.remove("SlideAndAppearR2M");
     addImages.classList.remove("hidden");
+  }, 980);
+}
+
+returnImagesSetup.onclick = returnToSetupImages;
+function returnToSetupImages() {
+  setUpProfileForm.classList.add("SlideAndFadeM2R");
+  setTimeout(() => {
+    addHobbies.classList.add("hidden");
+    setUpProfileForm.classList.remove("SlideAndFadeM2R");
+    setUpProfileForm.classList.add("SlideAndAppearL2M");
+  }, 490);
+  setTimeout(() => {
+    addImages.classList.remove("hidden");
+    setUpProfileForm.classList.remove("SlideAndAppearL2M");
   }, 980);
 }
 
@@ -29,5 +47,19 @@ function returnToBasicInfo() {
   setTimeout(() => {
     setUpBasicInfo.classList.remove("hidden");
     setUpProfileForm.classList.remove("SlideAndAppearL2M");
+  }, 980);
+}
+
+nextHobbiesSetup.onclick = nextToHobbiesSetup;
+function nextToHobbiesSetup() {
+  setUpProfileForm.classList.add("SlideAndFadeM2L");
+  setTimeout(() => {
+    addImages.classList.add("hidden");
+    setUpProfileForm.classList.remove("SlideAndFadeM2L");
+    setUpProfileForm.classList.add("SlideAndAppearR2M");
+  }, 490);
+  setTimeout(() => {
+    addHobbies.classList.remove("hidden");
+    setUpProfileForm.classList.remove("SlideAndAppearR2M");
   }, 980);
 }
