57536a8bf46f61a1ab03356998f6a40ded50d57d
Do Huy <doquochuy16905@gmail.com>
2025-04-19 15:26:58 +0700

    background auto scale and profile js transfer mini bug was removed

--- template/feature_profile_page/build/js/profile.js
+++ template/feature_profile_page/build/js/profile.js
@@ -119,38 +119,6 @@ function addUploadButton(container) {
             `;
     container.appendChild(label);
 }
-function transPage(navigationLink, section){
-    const settingsNavLinks = document.querySelectorAll('.'+navigationLink);//
-    const settingsSections = document.querySelectorAll('.'+section+'-section');//
-
-    if (settingsSections.length > 0) {
-        settingsSections[0].classList.remove('hidden');
-    }
-
-    settingsNavLinks.forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-
-            settingsNavLinks.forEach(l => l.classList.remove('active'));
-
-            link.classList.add('active');
-
-            settingsSections.forEach(section => {
-                section.classList.add('hidden');
-            });
-
-            const targetId = `${link.dataset.target}-section`;
-            const targetSection = document.getElementById(targetId);
-            if (targetSection) {
-                targetSection.classList.remove('hidden');
-            }
-
-            document.getElementById(section).classList.remove('hidden');
-        });
-    });
-}
-transPage('settings-nav-link','settings');
-transPage('settings-nav-general-link','general');
 document.addEventListener('DOMContentLoaded', function () {
     const sidebarLinks = document.querySelectorAll('aside nav a');
     const contentSections = document.querySelectorAll('.content-section');
