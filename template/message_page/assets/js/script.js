b742fd37f850f1f4c30259095d26569f212b9577
Thinh Nguyen <hongthinhnguyen789@gmail.com>
2025-05-21 09:06:25 +0700

    Message Page

--- template/message_page/assets/js/script.js
+++ template/message_page/assets/js/script.js
@@ -173,6 +173,37 @@ document.addEventListener('DOMContentLoaded', () => {
       chatContainer.innerHTML = renderMessages(messages);
     });
   });
+
+  //Search Box
+  const searchTriggers = document.querySelectorAll('.inner-search');
+  const boxSearch = document.getElementById('boxSearch');
+
+  searchTriggers.forEach(btn => {
+    btn.addEventListener('click', e => {
+      e.stopPropagation();
+      boxSearch.classList.toggle('hidden');
+    });
+  });
+
+  // Close
+  document.addEventListener('click', e => {
+    if (!boxSearch.contains(e.target) &&
+      ![...searchTriggers].some(t => t.contains(e.target))) {
+      boxSearch.classList.add('hidden');
+    }
+  });
+
+   document.addEventListener('click', (e) => {
+    if (btnBars.contains(e.target)) {
+      //Click Toggle Top Menu Icon
+      boxMenu.classList.toggle('hidden');
+    } else {
+      //Click outside from Toggle Top Menu Icon
+      if (!boxMenu.contains(e.target)) {
+        boxMenu.classList.add('hidden')
+      }
+    }
+  });
 });
 
 //
